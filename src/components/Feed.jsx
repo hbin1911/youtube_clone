6d9fc1385e1d8cedc7db50321e2562f6c2190d55
hbin1911 <rajas.mulik@gmail.com>
2023-09-10 19:41:24 +0530

    fixed error

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -36,7 +36,18 @@ const Feed = () => {
           Copyright 2023 Rajas
         </Typography>
       </Box>
-   
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
     </Stack>
   );
 };
